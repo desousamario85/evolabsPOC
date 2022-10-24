402188115b7a7f46b1bc584d8e7104dd44375eed
desousamario85 <desousa.mario@yahoo.com>
2022-10-24 21:17:13 +0000

    initial setup of Allauth

--- evo_labs/settings.py
+++ evo_labs/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    #AllAuth Apps
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +73,16 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+  
+]
+
+SITE_ID = 1
+
 
 WSGI_APPLICATION = 'evo_labs.wsgi.application'
 
