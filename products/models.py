68b6e439499d874438353308c42bc06c55f7870d
desousamario85 <desousa.mario@yahoo.com>
2023-04-15 20:08:07 +0000

    fixed categories spelling and fields listing in Admin

--- products/models.py
+++ products/models.py
@@ -19,6 +19,10 @@ class tech(DjangoChoices):
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name =  models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254 , null=True, blank=True)
 
